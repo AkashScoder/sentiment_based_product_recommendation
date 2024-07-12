c5e0bc691f80d4d477c7e1ff9010d3be46490d20
AKASH S <78740501+AkashScoder@users.noreply.github.com>
2024-07-12 18:03:07 +0530

    Update phone_list.py

--- phone_list.py
+++ phone_list.py
@@ -1,27 +1,28 @@
-lists = [[
-    'https://www.flipkart.com/vivo-v30-5g-classic-black-256-gb/product-reviews/itme3a94b78a025f?pid=MOBGYGCBHGAK8WGS&lid=LSTMOBGYGCBHGAK8WGSJSYWAR&marketplace=FLIPKART',
-    'vivo-v30-5g'],
+lists = [
     [
-        'https://www.flipkart.com/samsung-galaxy-s23-fe-purple-128-gb/product-reviews/itm03da6b42ff68e?pid=MOBGVTA24G7GHE6M&lid=LSTMOBGVTA24G7GHE6MN4I8LB&marketplace=FLIPKART',
+        'https://www.flipkart.com/vivo-v30-5g-classic-black-256-gb/p/itme3a94b78a025f?pid=MOBGYGCBHGAK8WGS&lid=LSTMOBGYGCBHGAK8WGSJSYWAR&marketplace=FLIPKART&q=www.flipkart.com%2Fvivo-v30-5g-classic-black-256-gb&store=tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=ca7a7f9f-642d-4502-b7f9-821fa0e4a633.MOBGYGCBHGAK8WGS.SEARCH&ppt=sp&ppn=sp&ssid=rahfrxcwpc0000001720768304441&qH=4883199576b54f18',
+        'vivo-v30-5g'],
+    [
+        'https://www.flipkart.com/samsung-galaxy-s23-fe-purple-128-gb/p/itm03da6b42ff68e?pid=MOBGVTA24G7GHE6M&lid=LSTMOBGVTA24G7GHE6MN4I8LB&marketplace=FLIPKART&q=samsung+galaxy+S23&store=tyy%2F4io&srno=s_1_3&otracker=search&otracker1=search&fm=Search&iid=47fae1d3-eb4a-49df-9367-50e737e79f19.MOBGVTA24G7GHE6M.SEARCH&ppt=sp&ppn=sp&ssid=099wrf380g0000001720768211661&qH=15b83e9c92a1fa60',
         'samsung-galaxy-s23'],
     [
-        'https://www.flipkart.com/poco-x3-pro-graphite-black-128-gb/product-reviews/itm9ce5166bf0e03?pid=MOBGFKNFRVPZ77GX&lid=LSTMOBGFKNFRVPZ77GXPOKQYD&marketplace=FLIPKART',
+        'https://www.flipkart.com/poco-x3-pro-steel-blue-128-gb/p/itm527548fcdf883?pid=MOBGFKNF6HFYZWPY&lid=LSTMOBGFKNF6HFYZWPYH0RJAD&marketplace=FLIPKART&q=poco+x3+pro&store=tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=551b593c-418c-4fdd-a248-9b0835bb3eda.MOBGFKNF6HFYZWPY.SEARCH&ppt=pp&ppn=pp&ssid=stwaxwtreo0000001720768384633&qH=5fa2a9dbeb17d9d2',
         'poco-x3-pro'],
     [
-        'https://www.flipkart.com/iqoo-neo-7-pro-fearless-flame-128-gb/product-reviews/itm1585345e64a32?pid=MOBGRHFZFJPGSQ9H&lid=LSTMOBGRHFZFJPGSQ9HQRZRLU&marketplace=FLIPKART',
-        'iqoo-neo-7-pro'],
+        'https://www.flipkart.com/apple-iphone-12-blue-64-gb/p/itm5778ad0d0d255?pid=MOBFWBYZ8DNJNY7N&lid=LSTMOBFWBYZ8DNJNY7NMWAWOJ&marketplace=FLIPKART&q=apple+12+&store=tyy%2F4io&srno=s_1_3&otracker=search&otracker1=search&fm=Search&iid=f1ede9b8-2c6c-4a08-ae80-b21c815febe9.MOBFWBYZ8DNJNY7N.SEARCH&ppt=sp&ppn=sp&ssid=9ez5iwjnu80000001720768140721&qH=e28470a0958e0761',
+        'apple-iphone-12'],
     [
-        'https://www.flipkart.com/oneplus-nord-ce4-celadon-marble-256-gb/product-reviews/itm5a09089114afb?pid=MOBGZZGY8FRH5Y7R&lid=LSTMOBGZZGY8FRH5Y7RE7OAXA&marketplace=FLIPKART',
+        'https://www.flipkart.com/oneplus-nord-ce4-celadon-marble-128-gb/p/itm5a09089114afb?pid=MOBGZN8YJ4KZ2KNH&lid=LSTMOBGZN8YJ4KZ2KNHHWWVQA&marketplace=FLIPKART&q=oneplus+nord+ce4&store=tyy%2F4io&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_1_15_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_1_15_na_na_ps&fm=Search&iid=6a45a802-b4aa-47b7-8b15-60b41789d4be.MOBGZN8YJ4KZ2KNH.SEARCH&ppt=sp&ppn=sp&ssid=9bzpr15i2o0000001720768440270&qH=be802f2b6dfde2b2',
         'oneplus-nord-ce4'],
     [
-        'https://www.flipkart.com/motorola-edge-50-pro-5g-68w-charger-luxe-lavender-256-gb/product-reviews/itmba8105d0d22ad?pid=MOBGXFXYMTKAMPTS&lid=LSTMOBGXFXYMTKAMPTSJQVWQM&marketplace=FLIPKART',
+        'https://www.flipkart.com/motorola-edge-50-pro-5g-68w-charger-luxe-lavender-256-gb/p/itmba8105d0d22ad?pid=MOBGXFXYMTKAMPTS&lid=LSTMOBGXFXYMTKAMPTSJQVWQM&marketplace=FLIPKART&q=motorola+edge+50+pro&store=tyy%2F4io&spotlightTagId=BestsellerId_tyy%2F4io&srno=s_1_2&otracker=AS_QueryStore_OrganicAutoSuggest_2_11_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_2_11_na_na_ps&fm=search-autosuggest&iid=7a155812-a11d-49d9-b5e0-8499e111b0d5.MOBGXFXYMTKAMPTS.SEARCH&ppt=sp&ppn=sp&ssid=8a2rmo18k00000001720768472372&qH=b3629d8b06502c5a',
         'motorola-edge-50-pro'],
     [
-        'https://www.flipkart.com/realme-12-pro-5g-navigator-beige-256-gb/product-reviews/itmcc78f150eeabd?pid=MOBGYQ6BVDHRJRSG&lid=LSTMOBGYQ6BVDHRJRSGDKJZZD&marketplace=FLIPKART',
+        'https://www.flipkart.com/realme-12-pro-5g-navigator-beige-256-gb/p/itmcc78f150eeabd?pid=MOBGYQ6BVDHRJRSG&lid=LSTMOBGYQ6BVDHRJRSGDKJZZD&marketplace=FLIPKART&q=realme+12+pro+5g&store=tyy%2F4io&srno=s_1_4&otracker=search&otracker1=search&fm=search-autosuggest&iid=00df3227-72b1-4a25-a2da-92d072cec478.MOBGYQ6BVDHRJRSG.SEARCH&ppt=sp&ppn=sp&ssid=lw59w9g6mo0000001720768524533&qH=1e49302487f3a7e6',
         'realme-12-pro-5g'],
 
     [
-        'https://www.flipkart.com/iqoo-z9-5g-brushed-green-256-gb/product-reviews/itm4bdb51f6a3f34?pid=MOBGZYZRNM766B5F&lid=LSTMOBGZYZRNM766B5FFVPS9H&marketplace=FLIPKART',
+        'https://www.flipkart.com/iqoo-z9-5g-brushed-green-256-gb/p/itm4bdb51f6a3f34?pid=MOBGZYZRNM766B5F&lid=LSTMOBGZYZRNM766B5F0M34WL&marketplace=FLIPKART&q=iqoo+neo+z9+5g&store=tyy%2F4io&srno=s_1_9&otracker=search&otracker1=search&fm=search-autosuggest&iid=f4bcba16-4b70-4352-88eb-2f27bc978f32.MOBGZYZRNM766B5F.SEARCH&ppt=sp&ppn=sp&ssid=hstzopbkpc0000001720768640457&qH=054e1bedcd44a88f',
         'iqoo-z9-5g']
 
 ]
